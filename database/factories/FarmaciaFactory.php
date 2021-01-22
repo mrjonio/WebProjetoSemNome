7a5ef1b04a1c8e928e43fe7ec8027ea40d4d3cb6
laisy <laisyferreira2011@gmail.com>
2021-01-21 22:40:49 -0300

    Ajustes nos Factories Endereco e Farmacia

--- database/factories/FarmaciaFactory.php
+++ database/factories/FarmaciaFactory.php
@@ -6,14 +6,14 @@ use App\Models\Farmacia;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-class UserFactory extends Factory
+class FarmaciaFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = Farmacia::class;
 
     /**
      * Define the model's default state.
