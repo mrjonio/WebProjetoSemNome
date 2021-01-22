5b9183f73e779a9eda52753c566fc75e1f4b7687
Carlos Antônio Alves Júnior <carlosaajunior.jp@gmail.com>
2021-01-22 00:34:18 -0300

    Mudando senha padrao

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'nome' => $this->faker->name,
             'email' => $this->faker ->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '123123123', // password
             'remember_token' => Str::random(10),
             'tipo_perfil' => $this->faker->randomElement(['Farmacia', 'Cliente']),
             'endereco_id' => $this->faker->numberBetween(1, 10),
