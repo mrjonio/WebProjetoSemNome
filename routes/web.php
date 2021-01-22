54b6c9735f3536111742bcc6ad5428722896a736
Carlos Antônio Alves Júnior <carlosaajunior.jp@gmail.com>
2021-01-22 05:15:40 -0300

    Rotas de criação/visualização/edição e logout de cliente

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\Auth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +19,23 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::get('/logout', [LoginController::class, 'logout']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('/cliente')->name('cliente')->namespace('Cliente')->group(function(){
+  Route::prefix('/criar')->name('.cadastrarCliente')->group(function () {
+      Route::get('/', [ClienteController::class, 'cadastrarCliente']);
+      Route::post('/salvar', [ClienteController::class, 'salvarCadastroCliente'])->name('.salvar');
+  });
+
+  Route::prefix('/editar')->name('.editarCliente')->group(function () {
+      Route::get('/', [ClienteController::class, 'editarCliente']);
+      Route::post('/salvar', [ClienteController::class, 'salvarEditarCliente'])->name('.salvar');
+  });
+
+});
+
+Route::prefix('/farmacia')->name('farmacia')->namespace('Farmacia')->group(function(){
+
+});
