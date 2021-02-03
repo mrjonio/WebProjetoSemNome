c0161b4c3b432ffdef258038939a84827181d981
laisy <laisyferreira2011@gmail.com>
2021-02-03 18:11:03 -0300

    Remover produto ok

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,11 @@ Route::prefix('/farmacia')->name('farmacia')->namespace('Farmacia')->group(funct
         Route::prefix('/mudar')->name('.editarDisponibilidadeProd')->group(function () {
             Route::get('/{id}', [FarmaciaController::class, 'editarDisponibilidadeProd'])->middleware('auth');
             });
-
+        
+        Route::prefix('/remover')->name('.removerProduto')->group(function () {
+            Route::get('/{id}', [FarmaciaController::class, 'removerProduto'])->middleware('auth');
+            Route::get('/remover/{id}', [FarmaciaController::class, 'salvarRemoverProduto'])->name('.remover')->middleware('auth');
+        });
     });
 
     Route::prefix('/remover')->name('.removerFarmacia')->group(function () {
